ecfe06706d1cf67a839e6aae019fe9b8d0eeb4a5
Daniels Culka <cdaniels038@gmail.com>
2025-01-10 00:10:59 +0200

    Rework courier dialogs and add shared layouts

---
+++ src/ProLab.App/Features/Couriers/Dialogs/CourierCreateDialogBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProLab.Shared.Couriers.Request;
+using Radzen;
+
+namespace ProLab.App.Features.Couriers.Dialogs;
+
+public class CourierCreateDialogBase : ComponentBase
+{
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    public ICourierService CourierService { get; set; }
+
+    public CreateCourierRequest Courier { get; set; } = new();
+
+    protected void Cancel()
+    {
+        DialogService.Close(false);
+    }
+
+    protected void Create()
+    {
+        _ = CourierService.CreateAsync(Courier);
+
+        DialogService.Close(true);
+    }
+}
---
+++ src/ProLab.App/Features/Couriers/Dialogs/CourierUpdateDialogBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using ProLab.Shared.Couriers.Request;
+using ProLab.Shared.Couriers.Response;
+using Radzen;
+
+namespace ProLab.App.Features.Couriers.Dialogs;
+
+public class CourierUpdateDialogBase : ComponentBase
+{
+    [Inject]
+    protected DialogService DialogService { get; set; }
+
+    [Inject]
+    public ICourierService CourierService { get; set; }
+
+    [Parameter]
+    public int CourierId { get; set; }
+
+    public UpdateCourierRequest Courier { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        GetCourierResponse courier = await CourierService.GetByIdAsync(CourierId);
+
+        Courier = new UpdateCourierRequest
+        {
+            FirstName = courier.FirstName,
+            LastName = courier.LastName
+        };
+    }
+
+    protected void Cancel()
+    {
+        DialogService.Close(false);
+    }
+
+    protected void Update()
+    {
+        _ = CourierService.UpdateAsync(CourierId, Courier);
+
+        DialogService.Close(true);
+    }
+}
