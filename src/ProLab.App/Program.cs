ef1aba902aef7e040789b6e43c8c2d88fa3b8f63
MartinsBoikovs <Martins.Boikovs@edu.rtu.lv>
2025-01-09 20:02:34 +0200

    .

--- src/ProLab.App/Program.cs
+++ src/ProLab.App/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5172") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:50007") });
 
 await builder.Build().RunAsync();
