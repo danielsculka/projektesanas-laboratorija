4d0554d61b75a82f73d81735ac45fe85b104c8b2
Daniels Culka <cdaniels038@gmail.com>
2025-01-12 23:16:52 +0200

    Fix ColorGenerator

--- src/ProLab.App/Shared/ColorGenerator.cs
+++ src/ProLab.App/Shared/ColorGenerator.cs
@@ -8,9 +8,9 @@ public class ColorGenerator
     {
         var random = new Random();
         var color = Color.FromArgb(
-            random.Next(0, 164),
-            random.Next(0, 164),
-            random.Next(0, 164));
+            random.Next(0, 196),
+            random.Next(0, 128),
+            random.Next(0, 136));
 
         return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
     }
